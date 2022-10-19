70d838fba08446ce669cb21faa607c13ecac8d63
ALLANKLEBER1983 <allankleber1983@gmail.com>
2022-10-19 09:58:07 -0300

    modificação nas classes de configurações

--- Compras/src/main/java/com/compras/service/PedidoService.java
+++ Compras/src/main/java/com/compras/service/PedidoService.java
@@ -5,10 +5,11 @@ import com.compras.repository.PedidoRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class PedidoService {
 
     private final PedidoRepository pedidoRepository;
